0c8c5263f7154b0ac95be5cfe37eac2be1ba3e94
Hieu <hi3unb@gmail.com>
2020-05-07 13:55:36 +0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# nextcloud
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name 'nextcloud'
+maintainer 'Nghiem Ba Hieu'
+maintainer_email 'hi3unb@gmail.com'
+license 'Apache-2.0'
+description 'Installs/Configures nextcloud'
+long_description 'Installs/Configures nextcloud'
+version '0.1.0'
+chef_version '>= 13.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/nextcloud/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/nextcloud'
